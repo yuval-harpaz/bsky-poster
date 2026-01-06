2ad309c5d0e7a47f87c1abd641fb0aeeb6e87fbd
yuval <yuval@lenovo.2020>
2026-01-06 23:37:15 +0200

    404

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bsky-web-poster/', // Assumes repo name is bsky-web-poster
+  base: '/bsky-poster/',
 })
